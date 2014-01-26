b78f5ee7792137bf51fd33041c500045ed4457d4
NikitaShulipa <nshulipa@gmail.com>
2014-01-26 23:25:25 +0200

    Added description for task #16

--- solutions/n016.js
+++ solutions/n016.js
@@ -1,3 +1,7 @@
+/*
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+What is the sum of the digits of the number 2^1000?
+*/
 define('n016', function () {
 	function n016_v1 (base, powerIndex) {
 		var baseNumber, countMultiplicand, answer, i, result;
