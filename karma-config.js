ef88ef1673cd4cda58c92b48b1bbd19552e80851
NikitaShulipa <nshulipa@gmail.com>
2014-01-26 22:37:45 +0200

    Added configuration files for TDD

---
+++ karma-config.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Wed Jan 22 2014 22:43:22 GMT+0200 (EET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '.',
+
+
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      {pattern: './solutions/*.js', included: false},
+      {pattern: './tests/*.js', included: false},
+      './require-config.js',
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      './karma-config.js',
+      '*.tmp',
+      '.subl*.tmp'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
