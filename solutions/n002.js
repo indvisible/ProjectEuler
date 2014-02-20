e8a38a5acb8ce2fcc1822571c526122fa728e5a1
NikitaShulipa <nshulipa@gmail.com>
2014-02-20 21:17:21 +0200

    Refactoring task #2. Create v2

--- solutions/n002.js
+++ solutions/n002.js
@@ -2,26 +2,49 @@
 By considering the terms in the Fibonacci sequence whose values do not exceed 
 four million, find the sum of the even-valued terms.
 */
-function n2(){
-	var first, second, current, sum;
-	first = 1;
-	second = 2;
-	sum = 2;
 
-	while(true){
-		current = first + second;
+define('n002', function() {
+	function n2_v1 (numberToCalculate) {
+		var first, second, current, sum;
+		first = 1;
+		second = 2;
+		sum = 2;
 
-		if (current <= 4000000) {
+		while (true) {
+			current = first + second;
+
+			if (current <= numberToCalculate) {
+				if (current % 2 === 0) {
+					sum += current;
+				};
+
+				first = second;
+				second = current;
+			} else {
+				break;
+			}
+		}
+
+		return sum;
+	}
+
+	function n2_v2 (numberToCalculate) {
+		var first = 1,
+			second = 2,
+			sum = 2,
+			current = 3;
+
+		for (; current <= numberToCalculate; current = first + second) {
 			if (current % 2 === 0) {
 				sum += current;
 			};
 
 			first = second;
 			second = current;
-		} else {
-			break;
 		}
+
+		return sum;
 	}
 
-	return sum;
-}
+	return n2_v2;
+});
