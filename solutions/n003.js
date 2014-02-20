bc6be929d12726c4cbd7be05048606fada046b29
NikitaShulipa <nshulipa@gmail.com>
2014-02-20 21:41:24 +0200

    Refactoring task #3. Added v2

--- solutions/n003.js
+++ solutions/n003.js
@@ -3,8 +3,8 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 define('n003', function() {
-    function n003_v1 (inputNumber) {
-        function isPrime (x) {
+    function n003_v1(inputNumber) {
+        function isPrime(x) {
             if (x === 1 || x === 2 || x === 3) {
                 return true;
             };
@@ -43,5 +43,41 @@ define('n003', function() {
         return result;
     }
 
-    return n003_v1;
+    function n003_v2(inputNumber) {
+        function isPrime(maybePrime) {
+            if (maybePrime === 1 || maybePrime === 2 || maybePrime === 3) {
+                return true;
+            };
+
+            var limit = parseInt(Math.sqrt(maybePrime), 10),
+                numberOfDivisors = 0,
+                counter = 1;
+
+            for (; counter < limit; counter++) {
+                if (maybePrime % counter === 0) {
+                    limit = maybePrime / counter;
+                    if (limit !== counter) {
+                        numberOfDivisors++;
+                    }
+
+                    numberOfDivisors++;
+                }
+
+                if (numberOfDivisors > 2) {
+                    return false;
+                };
+            }
+
+            return numberOfDivisors === 2;
+        }
+
+        var index = parseInt(Math.sqrt(inputNumber), 10);
+        for (; index > 1; index--) {
+            if (inputNumber % index === 0 && isPrime(index)) {
+                return index;
+            };
+        }
+    }
+
+    return n003_v2;
 });
