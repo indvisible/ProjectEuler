6255450eb28964553ce90aef88e6af6035b941e8
NikitaShulipa <nshulipa@gmail.com>
2014-02-20 21:15:23 +0200

    Modified solution for task #1

--- solutions/n001.js
+++ solutions/n001.js
@@ -4,13 +4,17 @@ The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-
-function n1_v1(){
-	var sum = 0;
-	for(int i = 3; i < 1000; i++){
-		if(i % 3 === 0 || i % 5 === 0){
-			sum +=i;
+define('n001', function () {
+	function n1_v1(number){
+		var sum, index;
+		sum = 0;
+		for(index = 3; index < number; index++){
+			if(index % 3 === 0 || index % 5 === 0){
+				sum += index;
+			}
 		}
+		return sum;
 	}
-	return sum;
-}
+
+	return n1_v1;
+});
