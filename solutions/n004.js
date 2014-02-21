01f3a01b322a2c368460c2efe9ac299f94345e01
NikitaShulipa <nshulipa@gmail.com>
2014-02-21 17:05:47 +0200

    Refactoring task #4. Added tests. Added v2

--- solutions/n004.js
+++ solutions/n004.js
@@ -4,7 +4,7 @@ The largest palindrome made from the product of two 2-digit numbers is 9009 = 91
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 define('n004', function() {
-    function n004_v1(maxNumber) {
+    function n004_v1(countDigits) {
         function isPalindrom(str) {
             str = '' + str;
             var isPalindrom = true;
@@ -19,6 +19,12 @@ define('n004', function() {
             return isPalindrom;
         }
 
+        var maxNumber = '9';
+        for (var i = countDigits - 1; i > 0; i--) {
+            maxNumber += '9';
+        };
+
+        maxNumber = parseInt(maxNumber, 10);
         var answer;
         var arr = [];
         for (var first = maxNumber; first >= 10; first--) {
@@ -34,5 +40,57 @@ define('n004', function() {
         return answer;
     }
 
-    return n004_v1;
+    function n004_v2(countDigits) {
+
+        // TODO: alter to separate file
+
+        // Explanation from http://stackoverflow.com/a/17800645/1563880
+        // Original solution from http://stackoverflow.com/a/5450113/1563880
+        function repeat(pattern, count) {
+            var result = '';
+            if (count >= 1) {
+                while (count > 0) {
+                    if (count & 1) {
+                        result += pattern;
+                    }
+                    count >>= 1, pattern += pattern;
+                }
+            }
+
+            return result;
+        }
+
+        function isPalindrom(input) {
+            input = '' + input;
+            var length = parseInt(input.length / 2, 10),
+                i = 0;
+
+            for (; i < length; i++) {
+                if (input[i] !== input[input.length - 1 - i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        var result = 1,
+            answer,
+            one = parseInt(repeat('9', countDigits), 10),
+            another,
+            endPoint = parseInt(Math.sqrt(one), 10);
+
+        for (; one >= endPoint; one--) {
+            for (another = one; another >= endPoint; another--) {
+                answer = one * another;
+                if (isPalindrom(answer)) {
+                    result = Math.max(answer, result);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    return n004_v2;
 });
--- tests/n004tests.js
+++ tests/n004tests.js
@@ -1,12 +1,12 @@
 define(['n004'], function(test) {
 	describe('Tests for 4 problem', function() {
 		describe('# should return', function() {
-			it('# 9009 when \'99\'', function() {
-				expect(test(99)).toEqual(9009);
+			it('# 9009 when \'2\'', function() {
+				expect(test(2)).toEqual(9009);
 			});
 
-			it('# 906609 when \'999\'', function() {
-				expect(test(999)).toEqual(906609);
+			it('# 906609 when \'3\'', function() {
+				expect(test(3)).toEqual(906609);
 			});
 		})
 	})
