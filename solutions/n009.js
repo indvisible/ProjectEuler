5b791385f86a94bc1193ebcf73be0c50c3e55414
NikitaShulipa <nshulipa@gmail.com>
2014-02-21 17:51:17 +0200

    Added tests for task #9

--- solutions/n009.js
+++ solutions/n009.js
@@ -3,30 +3,31 @@ A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
+define('n009', function() {
+    function n009_v1(numberToCalculate) {
+        function isSumEqualNumber(a, b, c, number) {
+            return a + b + c === number;
+        }
 
-function n9_v1(){
-    function isSumEqual1000(a, b, c){
-        return a + b + c === 1000;
-    }
-  
-    function checkPiphagorTheorem(a, b, c){
-        return a * a + b * b === c * c 
-            || a * a + c * c === b * b 
-            || b * b + c * c === a * a;
-    }
-  
-    for(var a = 1; a < 998; a++){
-        for(var b = 1; b < 998; b++){
-            for(var c = 1; c < 998; c++){
-                if(isSumEqual1000(a, b, c) && checkPiphagorTheorem(a, b, c)){
-                    return a * b * c;
+        function checkPiphagorTheorem(a, b, c) {
+            return a * a + b * b === c * c || a * a + c * c === b * b || b * b + c * c === a * a;
+        }
+
+        var limit = numberToCalculate - 2
+        for (var a = 1; a < limit; a++) {
+            for (var b = 1; b < limit; b++) {
+                for (var c = 1; c < limit; c++) {
+                    if (isSumEqualNumber(a, b, c, numberToCalculate) && checkPiphagorTheorem(a, b, c)) {
+                        return a * b * c;
+                    }
                 }
             }
         }
     }
-}
 
-// answer: 31875000
+    return n009_v1;
+});
+
 // a 200 
 // b 375 
-// c 425 
+// c 425
---
+++ tests/n009tests.js
@@ -0,0 +1,9 @@
+define(['n009'], function(test) {
+	describe('Tests for 9 problem', function() {
+		describe('# should return', function() {
+			it('# 31875000 when \'1000\'', function() {
+				expect(test(1000)).toEqual(31875000);
+			});
+		})
+	})
+});
