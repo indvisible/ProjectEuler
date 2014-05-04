bb8ddc8f197eceaed424dd887dc46cde8b98bb92
NikitaShulipa <nshulipa@gmail.com>
2014-05-04 22:10:34 +0300

    added tests for task #21

---
+++ tests/n021tests.js
@@ -0,0 +1,9 @@
+define(['n021'], function(test) {
+	describe('Tests for 21 problem', function() {
+		describe('# should return', function() {
+			it('# 71910 when \'220\'', function() {
+				expect(test(10000)).toEqual(71910);
+			});
+		})
+	})
+});
